993a2e70c4ae9c01a9ea139502705a3544aff1d6
Light4210 <tarnavskij2002@gmail.com>
2021-03-10 11:40:04 +0200

    some test

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('task/{id}/rate', [TaskController::class, 'rate'])->name('task.rate');
 });
 Route::redirect('home', '/');
-//test
-
+//testsss
